2ad506fc9a2eea0690ea71d227b76bf25099de57
Qotayba <qotayba.darawshi@gmail.com>
2024-08-19 23:19:29 +0300

    changes

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Common.Models
+{
+    public class Price
+    {
+        public decimal Amount {  get; private set; }
+        public string Currency { get; private set;}
+
+    }
+}
